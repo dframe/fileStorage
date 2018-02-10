6b707a26799378d25680d0d0914d7bee4a718334
Slawek Kaleta <s.kaleta@citycore.pro>
2018-02-10 22:07:10 +0100

    todo

--- tests/SetupTest.php
+++ tests/SetupTest.php
@@ -2,6 +2,12 @@
 use PHPUnit_Framework_Constraint_IsType as PHPUnit_IsType;
 use \Dframe\Database\WhereStringChunk;
 
+
+// backward compatibility
+if (!class_exists('\PHPUnit\Framework\TestCase') AND class_exists('\PHPUnit_Framework_TestCase')) {
+    class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
+}
+
 class SetupTest extends PHPUnit_Framework_TestCase {
 
 
