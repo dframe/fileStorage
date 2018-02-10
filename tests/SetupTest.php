eca49c5275a7bfee362ee1ff1013ad4b781c0432
Dusta <dusta@users.noreply.github.com>
2018-02-10 22:21:59 +0100

    Update SetupTest.php

--- tests/SetupTest.php
+++ tests/SetupTest.php
@@ -2,14 +2,7 @@
 namespace Dframe\tests;
 ini_set('session.use_cookies', 0);
 
-use PHPUnit_Framework_Constraint_IsType as PHPUnit_IsType;
-
-// backward compatibility
-if (!class_exists('\PHPUnit\Framework\TestCase') AND class_exists('\PHPUnit_Framework_TestCase')) {
-    class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
-}
-
-class SetupTest extends PHPUnit_Framework_TestCase {
+class SetupTest extends \PHPUnit\Framework\TestCase {
 
 
 	public function setUp(){
