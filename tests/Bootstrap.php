abdb7ce742e119fcf79b3828e30c883646a108e9
Sławomir Kaleta <slaszka@gmail.com>
2018-05-26 15:11:06 +0200

    fix namespace

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -1,3 +1,5 @@
 <?php
 $autoloader = include dirname(__DIR__) . '/vendor/autoload.php';
-$autoloader->addPsr4('Dframe\tests\\', __DIR__);
+$autoloader->addPsr4('Dframe\\FileStorage\\tests\\', __DIR__);
+
+
--- tests/SetupTest.php
+++ tests/SetupTest.php
@@ -2,20 +2,21 @@
 use PHPUnit_Framework_Constraint_IsType as PHPUnit_IsType;
 use \Dframe\Database\WhereStringChunk;
 
-
 // backward compatibility
-if (!class_exists('\PHPUnit\Framework\TestCase') AND class_exists('\PHPUnit_Framework_TestCase')) {
-    class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
+if (!class_exists('\PHPUnit\Framework\TestCase') and class_exists('\PHPUnit_Framework_TestCase')) {
+	class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
 }
 
-class SetupTest extends PHPUnit_Framework_TestCase {
+class SetupTest extends \PHPUnit\Framework\TestCase
+{
 
-
-	public function setUp(){
+	public function setUp()
+	{
     //ToDo
 	}
 
-	public function testToDo(){
-		
+	public function testToDo()
+	{
+
 	}
 }
