5fdf083108f3b88bd4317ee85ad55bddf95a92b3
Sławomir Kaleta <slaszka@gmail.com>
2018-12-17 21:21:47 +0100

    Resolved #24, add tests

--- tests/SetupTest.php
+++ tests/SetupTest.php
@@ -1,19 +1,14 @@
 <?php
-namespace Dframe\FileStorage\tests;
+namespace Dframe\FileStorage\Tests;
 
-ini_set('session.use_cookies', 0);
-
-// backward compatibility
-if (!class_exists('\PHPUnit\Framework\TestCase') and class_exists('\PHPUnit_Framework_TestCase')) {
-    class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
-}
+use PHPUnit\Framework\TestCase;
 
 /**
  * Class SetupTest
  *
  * @package Dframe\FileStorage\tests
  */
-class SetupTest extends \PHPUnit\Framework\TestCase
+class SetupTest extends TestCase
 {
     public function setUp()
     {
